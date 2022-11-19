09c34ce2a40648cd27e0adb5c2c8d6a9b11e4377
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-11-19 22:17:44 +0900

    Save firebase

--- src/pages/Add.tsx
+++ src/pages/Add.tsx
@@ -1,13 +1,48 @@
+import { doc, setDoc } from 'firebase/firestore'
+import { app } from '@common'
+import { useForm } from 'react-hook-form'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+import type { RootStates } from '@store'
+
+interface Inputs {
+  pos: string
+  excuse: string
+}
 
 const Add = () => {
   const navigate = useNavigate()
+  const { register, handleSubmit } = useForm<Inputs>()
+  const { user } = useSelector((state: RootStates) => ({
+    user: state.user
+  }))
 
   const onCancel = () => navigate(-1)
 
+  const onSubmit = (form: Inputs) => {
+    const date = new Date()
+    console.log(form)
+
+    const self_study = doc(
+      app.db,
+      'self-study',
+      `${user.sub}-${date.getMonth() + 1}-${date.getDate()}`
+    )
+    setDoc(self_study, {
+      ...form,
+      isChecked: false
+    })
+
+    navigate('/')
+  }
+
   return (
-    <div className="h-screen w-full bg-[#141717] text-[#FBFBFB] flex flex-col justify-center items-center">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="h-screen w-full bg-[#141717] text-[#FBFBFB] flex flex-col justify-center items-center"
+    >
       <div className="flex justify-between max-w-3xl w-[80%] mb-14">
         <button onClick={onCancel} className="text-[#2BA77A]">
           취소
@@ -20,7 +55,9 @@ const Add = () => {
           </p>
         </div>
 
-        <button className="text-[#2BA77A]">확인</button>
+        <button type="submit" className="text-[#2BA77A]">
+          확인
+        </button>
       </div>
 
       <div className="max-w-3xl w-[80%] mb-24">
@@ -28,6 +65,7 @@ const Add = () => {
         <div className="flex gap-2 items-center bg-[#8B8E8E] px-5 py-4 rounded-3xl shadow-lg shadow-white/20">
           <AiOutlineSearch color="4E4E55" size={25} />
           <input
+            {...register('pos', { required: true })}
             className="text-base bg-[#8B8E8E] placeholder:text-[#4e4e55] outline-none text-[#4E4E55] w-full"
             placeholder="어디서 자습을 하실건가요?"
           />
@@ -37,11 +75,12 @@ const Add = () => {
       <div className="max-w-3xl w-[80%] text-xl font-normal">
         <h2 className="text-xl font-normal mb-5">자습 사유</h2>
         <textarea
+          {...register('excuse', { required: true })}
           className="px-5 py-4 text-base bg-[#8B8E8E] placeholder:text-[#4e4e55] resize-none outline-none rounded-3xl shadow-lg shadow-white/20 w-full text-[#4e4e55]"
           placeholder="자습 사유를 입력해주세요."
         />
       </div>
-    </div>
+    </form>
   )
 }
 
