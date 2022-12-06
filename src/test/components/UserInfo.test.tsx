768716aff2b3b6dea99a6d3a9250c7c667368e6b
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-12-06 23:56:50 +0900

    Add UserInfo components

---
+++ src/test/components/UserInfo.test.tsx
@@ -0,0 +1,6 @@
+import { UserInfo } from '@components'
+import { render } from '@testing-library/react'
+
+test('', () => {
+  render(<UserInfo />)
+})
---
+++ test/components/UserInfo.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import UserInfo from '../../src/components/UserInfo'
+
+test('', () => {
+  render(<UserInfo />)
+})
